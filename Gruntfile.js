289a4882f6d8c7464dc58504fe437d6373ede271
David McClure <david.mcclure@virginia.edu>
2013-04-21 11:33:44 -0400

    Migrates to Grunt 0.4, simplifies build.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     simile-production
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.initConfig({
+
+    concat: {
+      dist: {
+        src: [
+          'dist/timeline_js/**/*.js',
+          'dist/timeline_ajax/**/*.js'
+        ],
+        dest: 'dist/production/simile.js'
+      }
+    },
+
+    cssmin: {
+      compress: {
+        files: {
+          "dist/production/simile.css": [
+            "dist/timeline_js/timeline-bundle.css",
+            "dist/timeline_ajax/styles/graphics.css"
+          ]
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['concat', 'cssmin']);
+
+};
